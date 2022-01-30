252518cab1a4d4a76bb64cdd9d75e9598c6cee07
NathanPeterson <nalexpeterson@gmail.com>
2022-01-30 14:40:10 -0500

    Style app, add Sega font

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import SearchBox from './SearchBox'
 import CardList from './CardList'
 import { robots } from './robots'
 
+import './App.css'
+
 class App extends React.Component {
     constructor(){
         super()
@@ -20,9 +22,10 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
+
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,5 +1,3 @@
-import react from "react";
-
 const SearchBox = ({searchfield, searchChange}) => {
     return (
         <div className='pa2'>
