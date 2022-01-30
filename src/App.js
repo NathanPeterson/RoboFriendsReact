69a4195bf06f6a16d0350cad8af76b3ccf6a0e90
NathanPeterson <nalexpeterson@gmail.com>
2022-01-30 14:55:05 -0500

    Retrieve data from API instead of hardcoded file

--- src/App.js
+++ src/App.js
@@ -9,9 +9,19 @@ class App extends React.Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        //Grab from file
+        //this.setState({ robots: robots})
+
+        //Grab from API
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {this.setState({robots: users})})
     }
 
     onSearchChange = (event) => {
@@ -23,13 +33,17 @@ class App extends React.Component {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
 
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+        if(this.state.robots.length === 0){
+            return <h1 className="tc">Loading . . .</h1>
+        }else{
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            )
+        }        
     }    
 }
 
