4dc046e4cafb86bdf8726161177d4c3a338a1af9
NathanPeterson <nalexpeterson@gmail.com>
2022-01-30 20:13:37 -0500

    gh-pages deploy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import SearchBox from './SearchBox'
-import CardList from './CardList'
-import Scroll from './Scroll'
+import SearchBox from '../components/SearchBox'
+import CardList from '../components/CardList'
+import Scroll from '../components/Scroll'
+import ErrorBoundry from "../components/ErrorBoundry";
 
 //Grab Data from file
 //import { robots } from './robots'
@@ -32,23 +33,25 @@ class App extends React.Component {
     }
 
     render (){
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield } = this.state;
+
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
 
-        if(this.state.robots.length === 0){
-            return <h1 className="tc">Loading . . .</h1>
-        }else{
-            return (
+        return !robots.length ? 
+           <h1 className="tc">Loading . . .</h1> :
+            (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
-            )
-        }        
+            )               
     }    
 }
 
